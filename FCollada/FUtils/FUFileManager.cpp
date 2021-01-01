ff827335065ea3aba6652628fa0ade46bedbc896
Matthew Lai <m@matthewlai.ca>
2021-01-01 20:29:02 +0000

    Removed call to formerly deprecated and now removed function.

--- FCollada/FUtils/FUFileManager.cpp
+++ FCollada/FUtils/FUFileManager.cpp
@@ -24,6 +24,7 @@
 #elif defined(__APPLE__)
 	#include <mach-o/dyld.h>
 	typedef int (*NSGetExecutablePathProcPtr)(char *buf, size_t *bufsize);
+	#include <sys/stat.h>
 #elif defined(LINUX)
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -210,7 +211,7 @@ bool FUFileManager::MakeDirectory(const fstring& directory)
 	if (_mkdir(TO_STRING(absoluteDirectory).c_str()) == 0) return true;
 	errno_t err; _get_errno(&err);
 	if (err == EEXIST) return true;
-#elif defined(LINUX)
+#elif defined(LINUX) || defined(__APPLE__)
 	if (mkdir(TO_STRING(absoluteDirectory).c_str(), ~0u) == 0) return true; // I think this means all permissions..
 #elif defined(__APPLE__)
 	fm::string _fname = TO_STRING(directory);
